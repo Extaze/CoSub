1f5415e6e5409ec0c814919814d3a1f3c2a917a9
Extaze <gabriel.juchault@gmail.com>
2014-09-09 00:30:33 +0200

    Login page

--- app/lang/en/cosub.php
+++ app/lang/en/cosub.php
@@ -4,7 +4,12 @@ return array(
 
     'subtitle'      => 'Collaborative Subtitling Tool',
     'lastestrooms'  => 'Lastest rooms',
-    'login'         => 'Login'
+    'login'         => 'Login',
+    'nickname'      => 'Nickname',
+    'password'      => 'Password',
+    'password2'     => 'Please re-enter your password',
+    'email'      => 'Email',
+    'email2'     => 'Please re-enter your email',
 );
 
 ?>
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -14,7 +14,41 @@
 
 @section('content')
     <h1>{{ trans('cosub.login') }}</h1>
-    <form method="post" role="form">
-
+    <form method="post" class="form-horizontal" role="form">
+        <div class="form-group">
+            <label for="nickname" class="col-sm-3 lato control-label">{{ trans('cosub.nickname') }} :</label>
+            <div class="col-sm-9">
+                <input type="text" name="nickname" id="nickname" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="password" class="col-sm-3 lato control-label">{{ trans('cosub.password') }} :</label>
+            <div class="col-sm-9">
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="password2" class="col-sm-3 lato control-label">{{ trans('cosub.password2') }} :</label>
+            <div class="col-sm-9">
+                <input type="password" name="password2" id="password2" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="email" class="col-sm-3 lato control-label">{{ trans('cosub.email') }} :</label>
+            <div class="col-sm-9">
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="email2" class="col-sm-3 lato control-label">{{ trans('cosub.email2') }} :</label>
+            <div class="col-sm-9">
+                <input type="email" name="email2" id="email2" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-9">
+                <button type="submit" class="btn btn-lg btn-block btn-success">Sign in</button>
+            </div>
+        </div>
     </form>
 @stop
