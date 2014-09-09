6bc92af23e810046b9d61e8059c9ec194aebcaf1
Extaze <gabriel.juchault@gmail.com>
2014-09-09 15:56:20 +0200

    Remember token on user table

--- app/database/migrations/2014_09_09_2_create_users_table.php
+++ app/database/migrations/2014_09_09_2_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration {
             $table->string('password');
             $table->string('email')->unique();
             $table->integer('language')->unsigned();
+            $table->rememberToken();
             $table->timestamps();
 
             $table->foreign('language')->references('id')->on('languages');
