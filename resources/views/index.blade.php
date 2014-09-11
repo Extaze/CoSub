3e5fb3a778e11d5dc03390b4b7f0392130ec5b60
Extaze <gabriel.juchault@gmail.com>
2014-09-11 23:01:31 +0200

    Show last rooms - view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>{{ trans('cosub.lastestrooms') }}</h1>
     @if ($errors->any())
         <p class="alert alert-danger">
             @foreach($errors->all() as $error)
                 {{$error}}<br>
             @endforeach
         </p>
+    @endif
+    <h1>{{ trans('cosub.lastestrooms') }}</h1>
+    @if (isset($yourRooms))
+        <ul class="yourRoomsList">
+            @foreach($yourRooms->all() as $yourRoom)
+                <li>
+                    <a href="{{ route('user.rooms') }}">{{ $yourRoom->name }}</a>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+    @if (isset($lastRooms))
+        <ul class="lastRoomsList">
+            @foreach($lastRooms->all() as $lastRoom)
+                <li>
+                    <a href="{{ route('rooms.room', [$lastRoom->id]) }}">{{ $lastRoom->name }}</a>
+                </li>
+            @endforeach
+        </ul>
     @endif
 @stop
