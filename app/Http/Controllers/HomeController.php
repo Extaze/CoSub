5e76de53de95a644fd059e40851dafce68cd09c2
Extaze <gabriel.juchault@gmail.com>
2014-09-11 23:01:15 +0200

    Show last rooms

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,11 +1,21 @@
 <?php namespace App\Http\Controllers;
 
+use App\Room;
+use Auth;
 use Illuminate\Routing\Controller;
 
 class HomeController extends Controller
 {
     public function getIndex()
     {
-        return view('index');
+        if (Auth::check())
+        {
+            return view('index', [
+                'yourRooms' => Room::take(10)->get()
+            ]);
+        }
+        return view('index', [
+            'lastRooms' => Room::take(10)->get()
+        ]);
     }
 }
